de1bb07d631bbc77f5588d26081a145d515a89e0
chauneng <50446006+chauneng@users.noreply.github.com>
2021-11-28 20:12:17 +0900

    11월 28일

--- chauneng/1946.py
+++ chauneng/1946.py
@@ -4,19 +4,13 @@ T = int(stdin.readline())
 
 for _ in range(T):
     N = int(stdin.readline())
-    arr = [list(map(int, stdin.readline().split())) for _ in range(N)]
-    arr.sort()
-
-    checked = []
+    applicant = [list(map(int, stdin.readline().split())) for _ in range(N)]
+    applicant.sort()
+    
+    cnt = 1
+    cutline = applicant[0][1]
     for i in range(N):
-        first, second = arr[i]
-        if second <= arr[0][1]:
-            checked.append((second, first))
-
-    cnt = 0
-    checked.sort()
-    for j in checked:
-        if j[1] <= checked[0][1]:
+        if cutline > applicant[i][1]:
             cnt += 1
-        
+            cutline = applicant[i][1]
     print(cnt)
