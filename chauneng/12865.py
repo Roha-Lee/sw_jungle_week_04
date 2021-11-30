cf8eb647e44c76e397e78a39acc7ff67140ea733
chauneng <50446006+chauneng@users.noreply.github.com>
2021-11-30 23:52:06 +0900

    숙제끝

--- chauneng/12865.py
+++ chauneng/12865.py
@@ -3,13 +3,10 @@ from sys import stdin
 N, K = map(int, stdin.readline().split())
 
 dp = [[0]*(K+1) for _ in range(N+1)]
-
 item = [[0]]
-
 for _ in range(N):
     item.append(list(map(int, stdin.readline().split())))
-
-item = sorted(item)
+item.sort()
 
 for i in range(1, N+1):
     for j in range(1, K+1):
@@ -20,4 +17,3 @@ for i in range(1, N+1):
             dp[i][j] = dp[i-1][j]
 
 print(dp[N][K])
-# print(sorted(item))
--- chauneng/2253.py
+++ chauneng/2253.py
@@ -2,29 +2,26 @@ from sys import stdin
 from collections import deque
 
 N, M = map(int, stdin.readline().split())
-SMALL = [int(stdin.readline()) for _ in range(M)]
-SMALL.sort()
-
+SMALL = set([int(stdin.readline()) for _ in range(M)])
+check = [[] for _ in range(N + 1)]
 dx = (-1, 0, 1)
-
 queue = deque()
-queue.append([1, 1, 0])
+queue.append([0, 1, 0])
 ans = -1
 while queue:
     speed, position, jump = queue.popleft()
-    if N == speed+position:
-        ans = jump
-        break
-
     for i in range(3):
-        newSpeed = speed+dx[i]
-        if newSpeed>0:
-            flag=True
-            for i in range(M):
-                if SMALL[i] == position+newSpeed:
-                    flag = False
-                    break
-            if flag:
-                queue.append([newSpeed, position+speed, jump+1])
+        newSpeed = speed + dx[i]
+        if newSpeed <= 0:
+            continue
+        new_pos = position + newSpeed
+        if new_pos == N:
+            ans = jump + 1 
+            print(ans)
+            exit(0)
+            
+        if new_pos not in SMALL and new_pos < N and newSpeed not in check[new_pos]: 
+            check[new_pos].append(newSpeed)
+            queue.append([newSpeed, new_pos, jump+1])
 
 print(ans)
